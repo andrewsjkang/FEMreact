6c506b80beabd61abac1dd07a80fd03ab2589279
Andrew Kang <andrewsjkang@gmail.com>
2018-08-24 16:36:41 -0700

    Implement search feature

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from "./Landing";
+import Search from "./Search";
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -10,6 +11,7 @@ const App = () => (
     <div className="app">
       <Switch>
         <Route exact path="/" component={Landing} />
+        <Route exact path="/search" component={Search} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
