3637209c82bcfdbdea49ecb3396f2b08a9f23bca
Andrew Kang <andrewsjkang@gmail.com>
2018-08-24 13:48:32 -0700

    Implement Landing page

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Landing from "./Landing";
 
-const App = () => <div>Hello World!</div>;
+const FourOhFour = () => <h1>404</h1>;
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+
+  </BrowserRouter>
+);
 
 render(<App />, document.getElementById("app"));
